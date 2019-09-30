056b71d8b82ac95bcf4abba2b92aa4100beba730
Joshua Taylor <jtaylor.rk@gmail.com>
2019-09-29 23:13:36 -0600

    9/29/2019

--- pageObjects/checkPointTwoPOM.js
+++ pageObjects/checkPointTwoPOM.js
@@ -1,7 +1,39 @@
 module.exports = {
     url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
     elements: {
+        evenInput: '[name="evenOddInput"]',
+        evenSubmit: '[name="evenOddButton"]',
+        evenRes: '[name="evenResults"]',
+        oddRes: '[name="oddResults"]',
+        objectInput: '[name="objectFilterInput"]',
+        objectSubmit: '[name="objectFilterButton"]',
+        objectRes: '[name="objectFilterResults"]',
+        stringInput: '#nameFilterInput',
+        stringSubmit: '#nameFilterButton',
+        stringRes: '[name="nameFilterResults"]',
+        palInput: '[name="palindromeInput"]',
+        palSubmit: '[name="palindromeButton"]',
+        palRes: '[name="palindromeResults"]',
+        sumInputOne: '[name="sumInput1"]',
+        sumInputTwo: '[name="sumInput2"]',
+        sumSubmit: '[name="sumButton"]',
+        sumRes: '[name="sumResults"]'
 
+    },
+
+    var: test = {
+        evenOdds: '1,2,3,4,5',
+        object: 'hairColor',
+        string: 'T',
+        pal: 'star rats',
+        palTwo: 'not Palindrome',
+        sumOne: '5',
+        sumTwo: '5'
     }
-
 }
+
+// var: errorInfo = {
+//     name: "lkjlkjlkjjlkjlkjlkjjlkjlkjlkjjl",
+//     phone: "1",
+//     title: "lkjlkjlkjjlkjlkjlkjjlkjlkjlkjjl"
+// }
---
+++ tests/checkPointTwo.js
@@ -0,0 +1,47 @@
+var pointTwo
+module.exports = {
+    before: browser => {
+        pointTwo = browser.page.checkPointTwoPOM()
+        pointTwo.navigate()
+    },
+    after: browser => {
+        browser.end()
+    },
+    'Palindrome': browser => {
+        pointTwo
+            .setValue('@palInput', test.pal)
+            .pause(2000)
+            .click('@palSubmit')
+            .verify.containsText('@palRes', 'true')
+            .clearValue('@palInput')
+            .setValue('@palInput', test.palTwo)
+            .click('@palSubmit')
+            .verify.containsText('@palRes', 'false')
+    },
+    'Odds and Evens': browser => {
+        pointTwo
+            .setValue('@evenInput', test.evenOdds)
+            .click('@evenSubmit')
+            .verify.containsText('@evenRes', '2,4')
+            .verify.containsText('@oddRes', '1,3,5')
+    },
+    'Filter Object': browser => {
+        pointTwo
+            .setValue('@objectInput', test.object)
+            .click('@objectSubmit')
+            .verify.containsText('@objectRes', 'hairColor')
+    },
+    'Filter String': browser => {
+        pointTwo
+            .setValue('@stringInput', test.string)
+            .click('@stringSubmit')
+            .verify.containsText('@stringRes', 'Tyler')
+    },
+    'Sum': browser => {
+        pointTwo
+            .setValue('@sumInputOne', test.sumOne)
+            .setValue('@sumInputTwo', test.sumTwo)
+            .click('@sumSubmit')
+            .verify.containsText('@sumRes', '10')
+    }
+}
